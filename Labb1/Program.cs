8d283d0d0827f35c20b923d283c1dda25bce4c39
PetterBostrom <petter.bostrom@gmail.com>
2023-08-31 15:08:24 +0200

    APIResponse implemented

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -1,6 +1,8 @@
+using AutoMapper;
 using Labb1;
 using Labb1.Data;
 using Labb1.Models;
+using Labb1.Models.DTOs;
 using Labb1.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -30,42 +32,96 @@ app.UseHttpsRedirection();
 
 app.MapGet("/books", async (IRepository<Book> bookRepo) =>
 {
-    return Results.Ok(await bookRepo.GetAll());
-});
+    APIResponse response = new APIResponse();
+
+    response.Result = await bookRepo.GetAll();
+    response.IsSuccess = true;
+    response.StatusCode = System.Net.HttpStatusCode.OK;
+
+    return Results.Ok(response);
+}).WithName("GetAllBooks").Produces(200);
 
 app.MapGet("/books/{id:int}", async (IRepository<Book> bookRepo, int id) =>
 {
-    return Results.Ok(await bookRepo.GetById(id));
-});
+    APIResponse response = new APIResponse();
 
-app.MapPost("/books", async (IRepository<Book> bookRepo, Book book) =>
-{
-    if (book != null)
-    {
-        await bookRepo.Create(book);
-        return Results.Ok(book);
-    }
-    return Results.BadRequest();
-});
+    response.Result = await bookRepo.GetById(id);
+    response.IsSuccess = true;
+    response.StatusCode = System.Net.HttpStatusCode.OK;
 
-app.MapPut("/books", async (IRepository<Book> bookRepo, Book book) =>
+    return Results.Ok(response);
+}).WithName("GetBook").Produces(200);
+
+app.MapPost("/books", async (
+    IMapper _mapper,
+    IRepository<Book> bookRepo, 
+    BookCreateDTO book_C_DTO) =>
 {
-    if (await bookRepo.GetById(book.Id) != null) 
+    APIResponse response = new() 
+    { IsSuccess = false, StatusCode = System.Net.HttpStatusCode.BadRequest };
+
+    var bookList = await bookRepo.GetAll();
+    bool alreadyExists = false;
+    foreach( var book in bookList)
     {
-        await bookRepo.Update(book);
-        return Results.Ok(book);
+        alreadyExists = BookComparer.Compare(book, book_C_DTO);
     }
-    return Results.NotFound("Book not Found");
-});
+
+    if (alreadyExists)
+    {
+        response.ErrorMessages.Add("Book already exists in database");
+        return Results.BadRequest(response);
+    }
+    
+    Book bookToAdd = _mapper.Map<Book>(book_C_DTO);
+
+    await bookRepo.Create(bookToAdd);
+
+    response.Result = bookToAdd;
+    response.IsSuccess = true;
+    response.StatusCode = System.Net.HttpStatusCode.Created;
+    return Results.Ok(response);
+    
+}).WithName("AddBook").Produces(201).Produces(400);
+
+app.MapPut("/books", async (
+    IMapper _mapper, 
+    IRepository<Book> bookRepo, 
+    Book book) =>
+{
+    APIResponse response = new() 
+    { IsSuccess = false, StatusCode = System.Net.HttpStatusCode.BadRequest };
+
+    if (await bookRepo.GetById(book.Id) == null)
+    {
+        response.ErrorMessages.Add($"Book with ID {book.Id} not found");
+        response.StatusCode = System.Net.HttpStatusCode.NotFound;
+        return Results.NotFound(response);
+    }
+
+    await bookRepo.Update(book);
+    response.Result = book;
+    response.IsSuccess = true;
+    response.StatusCode = System.Net.HttpStatusCode.Created;
+    return Results.Ok(response);
+}).WithName("UpdateBook").Produces(200).Produces(404);
 
 app.MapDelete("/books/{id:int}", async (IRepository<Book> bookRepo, int id) =>
 {
-    if (await bookRepo.GetById(id) != null)
+    APIResponse response = new()
+    { IsSuccess = false, StatusCode = System.Net.HttpStatusCode.BadRequest };
+
+    if (await bookRepo.GetById(id) == null)
     {
-        await bookRepo.Delete(id);
-        return Results.Ok();
+        response.ErrorMessages.Add($"Book with ID {id} not found");
+        response.StatusCode = System.Net.HttpStatusCode.NotFound;
+        return Results.NotFound(response);
     }
-    return Results.NotFound("Book not Found");
-});
+
+    await bookRepo.Delete(id);
+    response.IsSuccess= true;
+    response.StatusCode = System.Net.HttpStatusCode.NoContent;
+    return Results.Ok(response);
+}).WithName("DeleteBook").Produces(204).Produces(404);
 
 app.Run();
---
+++ Labb1/Services/BookComparer.cs
@@ -0,0 +1,29 @@
+﻿using Labb1.Models;
+using Labb1.Models.DTOs;
+
+namespace Labb1.Services
+{
+    public class BookComparer
+    {
+        public static bool Compare(Book book, BookCreateDTO bookCreateDTO)
+        {
+            if (book.Title == bookCreateDTO.Title)
+            {
+                if (book.Author == bookCreateDTO.Author)
+                {
+                    if (book.Description == bookCreateDTO.Description)
+                    {
+                        if (book.Year == bookCreateDTO.Year)
+                        {
+                            if (book.Genre == bookCreateDTO.Genre)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
